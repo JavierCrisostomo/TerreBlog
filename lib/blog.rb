17f8934e1540f6885ca95c9bf3f7f2ab1c1ddf6a
unknown <jcrisostomo@LT-PER-00055033.am.tmrk.corp>
2012-10-14 14:01:21 -0500

    JC: Cambiando el tipo de Aplicacion.

--- lib/blog.rb
+++ lib/blog.rb
@@ -1,9 +1,9 @@
-require 'sinatra/base'
+require 'sinatra'
 require 'ostruct'
 require 'time'
 require 'yaml'
 
-class Blog < Sinatra::Base
+class Blog < Sinatra::Application
   set :root, File.expand_path('../../', __FILE__)
   set :articles, []
   set :app_file, __FILE__
